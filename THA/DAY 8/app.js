2429dc2f938a7682899463fc37bdd86e346fd8a9
adityarawat1337x <napster98766@gmail.com>
2021-06-18 14:11:27 +0530

    day 8

--- THA/DAY 8/app.js
+++ THA/DAY 8/app.js
@@ -1,13 +1,25 @@
 const wrap = document.querySelector(".wrapper");
 
-for (let i = 0; i < 400; i++){
-    const temp = document.createElement("div")
-    temp.style.border = "1px solid white"
-    temp.style.width = "30px";
-    temp.style.height = "30px";
-    temp.addEventListener("click", () => {
-        temp.classList.toggle("white")
-    })
-    wrap.appendChild(temp);
-    }
+let click = 0;
 
+document.body.onmousedown = function () {
+	mouseDown = 1;
+};
+document.body.onmouseup = function () {
+	mouseDown = 0;
+};
+
+for (let i = 0; i < 400; i++) {
+	const temp = document.createElement("div");
+	temp.style.border = "1px solid white";
+	temp.style.width = "30px";
+    temp.style.height = "30px";
+		
+	temp.addEventListener("click", () => {
+		temp.classList.toggle("white");
+	});
+	temp.addEventListener("mouseover", () => {
+		if(mouseDown)temp.classList.toggle("white");
+	});
+	wrap.appendChild(temp);
+}
