63cb19a62284ef1d9c8b4f579d6e29add3aff723
adityarawat1337x <rkrawat98766@gmail.com>
2021-06-24 09:28:22 +0000

    updated

--- THA/DAY 10/app.js
+++ THA/DAY 10/app.js
@@ -1,14 +1,27 @@
 const cards = document.getElementsByClassName("card")
 let ids = []
+let allIds = ["a","b","c","d","e","f","a","b","c","d","e","f"]
 
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+shuffle(allIds);
+console.log(allIds);
+
+let i = 0;
 Array.from(cards).forEach(card => {
+    card.id = allIds[i];
+    i+=1;
     card.addEventListener("click",()=>{
         card.classList.toggle(`${card.id}`);
         if(ids.length===0)
             ids.push(card);
         else if(card.id == ids[0].id && card !=ids[0]){
             card.classList.add("solved")
-            ids[0].classList.add("solved")  
+            card.style.pointerEvents = 'none';
+            ids[0].classList.add("solved")
+            ids[0].style.pointerEvents = 'none';
             ids=[];
         }else{
             setTimeout(()=>{
