5e1332735846fbe2653c4c4300d2c17dc941c436
adityarawat1337x <rkrawat98766@gmail.com>
2021-07-02 20:54:56 +0530

    day 16 updated

--- THA/DAY 16/src/App.js
+++ THA/DAY 16/src/App.js
@@ -1,23 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
-  const [data,setData] = useState();
-  useEffect(async () => {
-    let db  = await fetch("https://meme-api.herokuapp.com/gimme");
-    db = await db.json()
-    setData(db)
+	const [data, setData] = useState();
+
+	const fetchData = async () => {
+		let db = await fetch("https://meme-api.herokuapp.com/gimme");
+		db = await db.json();
+    setData(db);
     console.log(db);
-  }, [])
-  return (
-    <div className="cont">
-      {data ? (<img src={ data.url} />) : ""}
-      {data ? (<h3>{ data.title}</h3>) : ""}
-      {data ? (<h4>-by { data.author}</h4>) : ""}
-    </div>
-  )
-}
+	};
 
-export default App
+	useEffect(fetchData, []);
 
+	return (
+		<>
+			<div className="cont">
+				{data ? <img src={data.url} /> : ""}
+				{data ? <h3>{data.title}</h3> : ""}
+				{data ? <h4>-by {data.author}</h4> : ""}
+			</div>
+			<button onClick={() => fetchData()}>Next</button>
+		</>
+	);
+};
+
+export default App;
