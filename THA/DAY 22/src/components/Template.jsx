24e8fe955482476b430088b55b2eff818d94bcde
adityarawat1337x <rkrawat98766@gmail.com>
2021-07-17 18:24:19 +0530

    Day 22

---
+++ THA/DAY 22/src/components/Template.jsx
@@ -0,0 +1,25 @@
+
+import React from "react";
+
+const Template = ({ templates, setMeme }) => {
+    return (
+        <>
+            {templates.map((template) => (
+                <div
+                    key={template.id}
+                    className="template"
+                    onClick={() => {
+                        setMeme(template);
+                    }}
+                >
+                    <div
+                        style={{ backgroundImage: `url(${template.url})` }}
+                        className="image"
+                    ></div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default Template;
