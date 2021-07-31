a1c882e84fba1a65627a3f66db2efd7a3957b596
adityarawat1337x <rkrawat98766@gmail.com>
2021-08-01 01:20:11 +0530

    Updates

---
+++ precache-manifest.e76e0e5b94805be6f9f8dabbde36ba85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac005e3ad765e97ae1fb",
+    "url": "/adityarawat1337x/FIRST_PWA/static/css/main.6fa6a671.chunk.css"
+  },
+  {
+    "revision": "ac005e3ad765e97ae1fb",
+    "url": "/adityarawat1337x/FIRST_PWA/static/js/main.65ea513d.chunk.js"
+  },
+  {
+    "revision": "9c86e8e0ee80085d29ae",
+    "url": "/adityarawat1337x/FIRST_PWA/static/js/runtime~main.7acf8361.js"
+  },
+  {
+    "revision": "ddca924bee50ff93f464",
+    "url": "/adityarawat1337x/FIRST_PWA/static/js/2.351ca56c.chunk.js"
+  },
+  {
+    "revision": "a9fb68f8a2b58de6a43163bba3db8af2",
+    "url": "/adityarawat1337x/FIRST_PWA/index.html"
+  }
+];
